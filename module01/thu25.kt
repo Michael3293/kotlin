4c7dd4fdc49252d47bcf2d055cb3a7595a20f2d7
Michael3293 <157508636+Michael3293@users.noreply.github.com>
2024-01-25 11:10:37 -0600

    save old version

---
+++ module01/thu25.kt
@@ -0,0 +1,14 @@
+ const val HERO_NAME = "Madrigal"
+
+fun main() {
+    println("The hero announces her presence to the world")
+    
+    println(HERO_NAME)
+
+    var playerlevel = 4
+    println(playerlevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword")
+    playerlevel += 1
+    println(playerlevel)
+}
