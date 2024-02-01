6182c9d76e7b982d93e3dbe233e186285db52005
Michael3293 <157508636+Michael3293@users.noreply.github.com>
2024-02-01 09:55:27 -0600

    create println code

---
+++ week04/module04/println.kt
@@ -0,0 +1,10 @@
+// HelloWorld/Task1.kt
+package helloWorldITSE1329
+
+// TODO
+
+fun main() {
+    println("Hello World!")
+    println("My name is Michael Bankston")
+    println("I am taking kotlin ITSE1329-82G01")
+}
